c87def8aee8011ca7c30cf362715d248c47cbe1f
vivekr.dr <vivek.reddy@transformuk.com>
2017-08-25 14:40:00 +0100

    * updated tests after master rebase

--- tests/home/steps/defendant.js
+++ tests/home/steps/defendant.js
@@ -1,11 +1,14 @@
 'use strict'
 
-let defendantTypePage, defendantAddressPage
+let defendantTypePage, defendantAddressPage, defendantRepresentativePage, defendantRepresentativeAddressPage, defendantAddAnotherDefendantPage
 
 module.exports = {
   _init () {
     defendantTypePage = require('../../defence/pages/defendant-type')
     defendantAddressPage = require('../../defence/pages/defendant-address')
+    defendantRepresentativePage = require('../../defence/pages/defendant-represented')
+    defendantRepresentativeAddressPage = require('../../defence/pages/defendant-reps-address')
+    defendantAddAnotherDefendantPage = require('../../defence/pages/defendant-add')
   },
 
   enterDefendantTypeIndividual () {
@@ -17,6 +20,27 @@ module.exports = {
 
   enterDefendantAddress () {
     defendantAddressPage.enterYourOrganisationAddress()
+  }
+  ,
+
+  enterDefendantRepsCompanyName () {
+    defendantRepresentativePage.enterDefendantCompanyName()
+  },
+
+  noDefendantCompanyName () {
+    defendantRepresentativePage.noDefendantCompanyName()
+  },
+
+  enterDefendantRepsAddress () {
+    defendantRepresentativeAddressPage.enterDefendantRepsCompanyAddress()
+  },
+
+  enterAnotherDefendant () {
+    defendantAddAnotherDefendantPage.enterAnotherDefendant()
+  },
+
+  noAnotherDefendant () {
+    defendantAddAnotherDefendantPage.noAnotherDefendant()
   }
 
 }
