f3fed0b9172c539d144066940930d7122bc99d62
vivekr.dr <vivek.reddy@transformuk.com>
2017-08-25 13:04:15 +0100

    * Added todo pages

---
+++ tests/amountClaim/pages/total.js
@@ -0,0 +1,24 @@
+'use strict'
+/* global actor */
+
+let I
+
+module.exports = {
+
+  _init () {
+    I = actor()
+  },
+  fields: {
+    addressLine1: 'input[id=line1]',
+    addressLine2: 'input[id=line2]',
+    cityName: 'input[id=city]',
+    postcode: 'input[id=postcode]'
+  },
+  buttons: {
+    saveandContinue: 'input.button'
+  },
+
+  open () {
+    I.amOnPage('/claim/total')
+  }
+}
---
+++ tests/home/steps/amountClaims.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let personalInjuryPage, housingDisrepairPage, summariseTheClaimPage, amountPage, totalPage, detailsSummaryPage, statementOfTruthPage, payByAccountPage, submittedPage
+
+module.exports = {
+  _init () {
+    personalInjuryPage = require('../../amountClaim/pages/personal-injury')
+    housingDisrepairPage = require('../../amountClaim/pages/housing-disrepair')
+    summariseTheClaimPage = require('../../amountClaim/pages/summarise-the-claim')
+    amountPage = require('../../amountClaim/pages/amount')
+    totalPage = require('../../amountClaim/pages/total')
+    detailsSummaryPage = require('../../amountClaim/pages/details-summary')
+    statementOfTruthPage = require('../../amountClaim/pages/statement-of-truth')
+    payByAccountPage = require('../../amountClaim/pages/pay-by-account')
+    submittedPage = require('../../amountClaim/pages/submitted')
+  },
+
+  openPage  () {
+    personalInjuryPage.open()
+    housingDisrepairPage.open()
+    summariseTheClaimPage.open()
+    amountPage.open()
+    totalPage.open()
+    detailsSummaryPage.open()
+    statementOfTruthPage.open()
+    payByAccountPage.open()
+    submittedPage.open()
+  }
+
+}
