58a7c23c92608e8b0c6582502a3fbf5132ad8081
Robert Parkinson <robertparkinson@users.noreply.github.com>
2017-09-05 11:41:52 +0100

    Feature/updated reps name (#46)
    * update content on page
    
    * update names to reflect page better
    
    * missed one
    
    * updated function name
    
    * update last remaining places

--- tests/home/steps/user.js
+++ tests/home/steps/user.js
@@ -2,14 +2,14 @@
 
 const user = require('../../../test-data').user
 
-let loginPage, startClaimPage, yourDetailsPage, yourOrganisationAddressPage, yourContactDetailsPage, yourReferencePage, yourCountyCourtPage, claimantTypePage, claimantAddressPage
+let loginPage, startClaimPage, enterYourOrganisationNamePage, yourOrganisationAddressPage, yourContactDetailsPage, yourReferencePage, yourCountyCourtPage, claimantTypePage, claimantAddressPage
 
 module.exports = {
 
   _init () {
     loginPage = require('../pages/login')
     startClaimPage = require('../../claim/pages/claimant-start-claim')
-    yourDetailsPage = require('../../claim/pages/claimant-representative-name')
+    enterYourOrganisationNamePage = require('../../claim/pages/claimant-representative-name')
     yourOrganisationAddressPage = require('../../claim/pages/claimant-representative-address')
     yourContactDetailsPage = require('../../claim/pages/claimant-representative-contacts')
     yourReferencePage = require('../../claim/pages/claimant-reference')
@@ -25,8 +25,8 @@ module.exports = {
     startClaimPage.open()
     startClaimPage.startClaim()
   },
-  enterYourDetails () {
-    yourDetailsPage.enterYourDetails()
+  enterYourOrganisationNamePage () {
+    enterYourOrganisationNamePage.enterYourOrganisationNamePage()
   },
   enterYourOrganisationAddress () {
     yourOrganisationAddressPage.enterYourOrganisationAddress()
