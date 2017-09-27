dd15755519121bdba0fe8bbe809a61bdb96eb591
vivekr.dr <vivek.reddy@transformuk.com>
2017-09-27 13:31:39 +0100

    *  updated steps

--- tests/amountClaim/pages/submitted.js
+++ tests/amountClaim/pages/submitted.js
@@ -14,7 +14,7 @@ module.exports = {
 
   },
   buttons: {
-    saveAndContinue: 'input.button.button-start'
+    finish: 'input.button.button-start'
   },
 
   open () {
@@ -29,6 +29,14 @@ module.exports = {
     I.see('1. Download the sealed claim form (this will open in a new window).')
     I.scrollPageToBottom()
     I.click('Download the sealed claim form')
-    I.wait(10)
+    I.waitForText('2. Send the form, particulars of claim and a response pack to the defendant within 4 months of the date of issue.')
+    I.waitForText('3. Send the court a certificate of service and a copy of any documents you served on the defendant, within 21 days of service.')
+    I.see('Court address:')
+    I.see('County Court Money Claims Centre (CCMCC)')
+    I.see('Salford')
+    I.see('Greater Manchester')
+    I.see('M5 0BY')
+    I.see('DX: 702634 Salford 5')
+    I.click(this.buttons.finish)
   }
 }
