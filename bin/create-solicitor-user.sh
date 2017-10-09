664eb9aa1c744da833bcabe29019fb6fec241c14
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-10-09 10:54:00 +0100

    Upgrade Idam-app version (#70)

---
+++ bin/create-solicitor-user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+USER_EMAIL="${1:-me@server.net}"
+FORENAME="${2:-John}"
+SURNAME="${3:-Smith}"
+PASSWORD="${4:-Password12}"
+
+curl -XPOST -H 'Content-Type: application/json' http://localhost:8080/testing-support/accounts -d '{
+    "email": "'${USER_EMAIL}'",
+    "forename": "'${FORENAME}'",
+    "surname": "'${SURNAME}'",
+    "levelOfAccess": 0,
+    "userGroup": {
+      "code": "cmc-solicitor"
+    },
+    "activationDate": "",
+    "lastAccess": "",
+    "password": "'${PASSWORD}'"
+}'
+
+echo -e "Created user with:\nUsername: ${USER_EMAIL}\nPassword:${PASSWORD}\nFirstname: ${FORENAME}\nSurname: ${SURNAME}"
